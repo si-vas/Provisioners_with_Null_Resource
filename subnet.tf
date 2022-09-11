94189a173f06b60e4366887c4fb0dd5ae722520b
siva <ssiva22292@gmail.com>
2022-09-11 12:43:23 -0400

    subnet added

---
+++ subnet.tf
@@ -0,0 +1,12 @@
+resource "aws_subnet" "public-subnet" {
+  count  = var.public_cidrs == var.azs ? 3 : 1
+  vpc_id = aws_vpc.main.id
+  #cidr_block = var.public_cidrs
+  cidr_block = element(var.public_cidrs, count.index)
+  #availability_zone = ap-souh-1a
+  availability_zone = element(var.azs, count.index)
+  tags = {
+    #Name = "${var.vpc_name}Terraform-Public-Subnet "
+    Name = "${var.vpc_name}Terraform-Public-Subnet ${count.index + 1} "
+  }
+}
