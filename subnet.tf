5fb4028efa4608db135aa1c5b9021ee639d3c756
si-vas <99237926+si-vas@users.noreply.github.com>
2022-09-11 23:30:27 -0400

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -1,5 +1,5 @@
 resource "aws_subnet" "public-subnet" {
-  count  = var.public_cidrs != var.azs ? 3 : 1
+  count  = var.public_cidrs != var.azs ? 2 : 1
   vpc_id = aws_vpc.main.id
   #cidr_block = var.public_cidrs
   cidr_block = element(var.public_cidrs, count.index)
