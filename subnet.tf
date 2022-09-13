0622b0673b60151fa6002461e1e96274a6fe3e0b
siva <ssiva22292@gmail.com>
2022-09-13 01:06:27 -0400

    private subnet modified

--- subnet.tf
+++ subnet.tf
@@ -10,3 +10,16 @@ resource "aws_subnet" "public-subnet" {
     Name = "${var.vpc_name}Terraform-Public-Subnet ${count.index + 1} "
   }
 }
+
+resource "aws_subnet" "private-subnet" {
+  count  = var.public_cidrs != var.azs ? 2 : 1
+  vpc_id = aws_vpc.main.id
+  #cidr_block = var.private_cidrs
+  cidr_block = element(var.private_cidrs, count.index)
+  #availability_zone = "ap-south-1a"
+  availability_zone = element(var.azs, count.index)
+  tags = {
+    #Name = "${var.vpc_name}Terraform-Private-Subnet "
+    Name = "${var.vpc_name}Terraform-Private-Subnet ${count.index + 1} "
+  }
+}
