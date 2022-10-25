2b448d471d87892b2e6c7fb624ea6c71d75787af
si-vas <99237926+si-vas@users.noreply.github.com>
2022-10-25 02:17:13 -0400

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 sudo apt -y update
 sudo apt install nginx -y
-sudo rm -rf webhooktesting
-sudo git clone https://github.com/mavrick202/webhooktesting.git
+sudo rm -rf NginX-Testing
+sudo git clone https://github.com/si-vas/NginX-Testing.git
 sudo rm -rf /var/www/html/index.nginx-debian.html
-sudo cp webhooktesting/index.html /var/www/html/index.nginx-debian.html
+sudo cp NginX-Testing/index.html /var/www/html/index.nginx-debian.html
 sudo chmod 777 /var/www/html/index.nginx-debian.html
-sudo cp webhooktesting/style.css /var/www/html/style.css
-sudo cp webhooktesting/scorekeeper.js /var/www/html/scorekeeper.js
+sudo cp NginX-Testing/style.css /var/www/html/style.css
+sudo cp NginX-Testing/scorekeeper.js /var/www/html/scorekeeper.js
 sudo echo "<center> <div id='container'> <h1> Welcome to public server </h1></div></center>" >> /var/www/html/index.nginx-debian.html
